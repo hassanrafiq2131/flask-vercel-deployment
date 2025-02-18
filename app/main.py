4e874992ccb10a630253fd755339afee19cfd45b
hassanrafiq2131 <i200734@nu.edu.pk>
2025-02-18 23:22:50 +0500

    making changes

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Deploying Flask App at Vercel"
+    return "Re-Deploying Flask App at Vercel with changes"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000)
